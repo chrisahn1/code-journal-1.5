82b048500b05c72f1ba13502a8b7cb73c83c5bf7
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2022-02-14 17:13:35 -0800

    task 5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 /* global data */
 /* exported data */
+var createEntryForm = document.querySelector('#create-entry-form');
+var inputs = document.querySelector('#create-entry-form').elements;
+var photoInput = document.querySelector('#photo-url');
+var image = document.querySelector('img');
+
+photoInput.addEventListener('input', changePhoto);
+
+function changePhoto(event) {
+  image.src = inputs[1].value;
+}
+
+createEntryForm.addEventListener('submit', createEntry);
+
+function createEntry(event) {
+  event.preventDefault();
+
+  createEntryForm.reset();
+}
