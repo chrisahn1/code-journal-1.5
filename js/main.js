0ef4b9d3cec97291be908018fd7162a8642c67c8
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2022-02-17 03:21:51 -0800

    Tasks 4, 7-15

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ var viewEntriesButton = document.querySelector('.view-entries-button');
 
 var viewEntryPage = document.querySelector('.container-view-entries');
 var newButton = document.querySelector('.new-button');
-// var viewEntryForm = document.querySelector('#view-entries-form');
 
 photoInput.addEventListener('input', changePhoto);
 
@@ -35,6 +34,12 @@ function createEntry(event) {
   image.src = 'images/placeholder-image-square.jpg';
 
   document.querySelector('#create-entry-form').reset();
+
+  var ul = document.querySelector('.entries-list');
+  ul.appendChild(entryTree(data.entries[0]));
+
+  createEntryPage.className = 'container hidden';
+  viewEntryPage.className = 'container-view-entries';
 }
 
 viewEntriesButton.addEventListener('click', viewEntries);
@@ -51,25 +56,31 @@ function newEntry(event) {
   viewEntryPage.className = 'container-view-entries hidden';
 }
 
-window.addEventListener('beforeunload', entryTree);
-
 function entryTree(entry) {
+
+  var emptyView = document.querySelector('.empty-view');
+  if (data.entries.length > 0) {
+    emptyView.className = 'empty-view hidden';
+  } else {
+    emptyView.className = 'empty-view';
+  }
+
   var li = document.createElement('li');
   li.setAttribute('class', 'entry-row');
   var photo = document.createElement('div');
   var image = document.createElement('img');
 
-  photo.setAttribute('class', 'photo-url');
-  image.setAttribute('class', 'photo');
+  photo.setAttribute('class', 'entry-photo');
+  image.setAttribute('class', 'entry-photo-url');
   image.setAttribute('src', entry.photoURL);
 
   photo.appendChild(image);
 
   var titleDescription = document.createElement('div');
-  var title = document.createElement('h2');
+  var title = document.createElement('h3');
   var description = document.createElement('p');
 
-  titleDescription.setAttribute('class', 'title-photo');
+  titleDescription.setAttribute('class', 'entry-title-description');
   title.setAttribute('class', 'entry-row-title');
   description.setAttribute('class', 'entry-row-description');
 
@@ -85,8 +96,12 @@ function entryTree(entry) {
   return li;
 }
 
-var ul = document.querySelector('.entries-list');
+function loadingEntries() {
+  var ul = document.querySelector('.entries-list');
 
-for (var i = 0; i < data.entries.length; i++) {
-  ul.appendChild(entryTree(data.entries[i]));
+  for (var i = 0; i < data.entries.length; i++) {
+    ul.prepend(entryTree(data.entries[i]));
+  }
 }
+
+loadingEntries();
